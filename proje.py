d1ec1c18487ba6aa361f2fa6eacfbc3e99104eb9
OnurArslan <onurarslanj@gmail.com>
2022-05-15 15:09:37 +0300

    usersqlıte fonk oluşturuldu veritabanı ile kullanıcı kaydı alındı

--- proje.py
+++ proje.py
@@ -1,4 +1,5 @@
 import datetime
+import sqlite3
 
 def hellouser():
     h="Biletgo'ya hoş geldiniz"
@@ -8,6 +9,32 @@ def hellouser():
     tarih = datetime.datetime.strftime(saatgun, '%c')
     info=f"{tarih}".center(40,'*')
     print(info)
-    
-hellouser()
 
+def usersqlite():
+    user=[]
+    print("-Bilet Alma Menüsü-")
+    ad=input("Adınızı giriniz:")
+    soyad=input("soyadınızı giriniz:")
+    tc=input("Tc no giriniz:")
+    numara=input("telefon numarası giriniz")
+    user.append((ad,soyad,tc,numara))
+    with sqlite3.connect('vt.sqlite') as vt:
+        im=vt.cursor()
+        im.execute("""CREATE TABLE IF NOT EXISTS users(ad,soyad,tc,numara)""")
+        for x in user:
+            im.execute("""INSERT INTO users VALUES (?,?,?,?)""",x)
+            vt.commit() 
+    print("kaydınız tamamlanmıştır.")
+    
+def menu():
+    while True:
+       print("""
+    
+       -bilet almak için (1)
+       -BiletGo sıkça sorulan sorular için (2)
+       -Gün sonu raporu(sadece yetkililer) (3)
+       """)
+       print("\n \n")
+       karar=int(input("   tuşlayınız:"))
+       print("\n \n")
+       usersqlite()
